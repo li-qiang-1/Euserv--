81f30f57737b7996f959be3778f8f4805f38c767
chen wei <c123c1zz1@gmail.com>
2024-11-14 15:51:46 +0800

    EUservAuto.py

--- EUservAuto.py
+++ EUservAuto.py
@@ -18,8 +18,8 @@ PASSWORD = os.environ["PASSWORD"]  # 密码
 
 # TrueCaptcha https://apitruecaptcha.org
 # 验证码识别，默认使用 Demo API，每天有100次免费额度，建议自行注册以确保稳定性
-USERID = os.environ["USERID"]
-APIKEY = os.environ["APIKEY"]
+USERID = "c123c"  # TrueCaptcha 用户ID
+APIKEY = "fhPZR431uVRRGFPoYFDJ"  # TrueCaptcha API密钥
 TRUECAPTCHA_CHECK_USAGE = True
 
 # Telegram Bot Push https://core.telegram.org/bots/api#authorizing-your-bot
